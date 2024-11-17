a7753705de232d6bc8be6f5ffa4a9f58a114a5ee
Maksim Georgiev Marinov <mmarinov@car11s6.42barcelona.com>
2024-11-17 20:01:11 +0100

    resolve conflicts

--- src/pipex.h
+++ src/pipex.h
@@ -6,8 +6,22 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 14:57:37 by mmarinov          #+#    #+#             */
-/*   Updated: 2024/11/13 14:57:40 by mmarinov         ###   ########.fr       */
+/*   Updated: 2024/11/15 14:54:21 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
 
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+
+void	ft_putstr_fd(char *s, int fd);
+void	handle_error(char *msg);
+
+#endif
